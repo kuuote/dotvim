69b8109b53e6366289db6b603c51c1653889d667
kuuote <znmxodq1@gmail.com>
2022-10-09 22:19:40 +0900

    孤独なクォート

--- lua/vimrc/map.lua
+++ lua/vimrc/map.lua
@@ -1,19 +1,19 @@
-local callback = require("vimrc.callback")
+local callback = require('vimrc.callback')
 
 local M = {}
 
 M.fns = {}
 local fnidx = 1
 
-if vim.fn.has("nvim") == 1 then
+if vim.fn.has('nvim') == 1 then
   M.define = vim.keymap.set
 else
   M.define = function(mode, lhs, rhs, opts)
-    if type(mode) == "string" then
+    if type(mode) == 'string' then
       mode = { mode }
     end
 
-    if type(rhs) == "function" then
+    if type(rhs) == 'function' then
       local id = callback.register(id)
       M.fns[fnidx] = rhs
       rhs = string.format("<Cmd>lua require('vimrc.callback').call(%d)<CR>", fnidx)
@@ -21,16 +21,16 @@ else
     end
 
     opts = opts or {}
-    local mods = ""
+    local mods = ''
 
-    for _, m in ipairs({ "buffer", "nowait", "silent", "script", "expr" }) do
+    for _, m in ipairs({ 'buffer', 'nowait', 'silent', 'script', 'expr' }) do
       if opts[m] then
-        mods = mods .. "<" .. m .. ">"
+        mods = mods .. '<' .. m .. '>'
       end
     end
 
     for _, m in ipairs(mode) do
-      local cmd = string.format("%snoremap %s %s %s", m, mods, lhs, rhs)
+      local cmd = string.format('%snoremap %s %s %s', m, mods, lhs, rhs)
       vim.command(cmd)
     end
   end
