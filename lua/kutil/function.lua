9a65e5e10c4dff5d28b548fdd7314061328c907c
kuuote <znmxodq1@gmail.com>
2022-10-22 19:01:51 +0900

    キーワード付き配列を分解するやつ

--- lua/kutil/function.lua
+++ lua/kutil/function.lua
@@ -1,11 +1,26 @@
 local M = {}
 
 function M.map(tbl, fn)
-	local new = {}
+  local new = {}
   for _, v in ipairs(tbl) do
     table.insert(new, fn(v))
   end
   return new
+end
+
+-- キーワード付き配列を分解するやつ
+-- {1, 2, a = 3} => {1, 2}, {a = 3}
+function M.partition_keyvalue(tbl)
+  local ip = {}
+  local kv = {}
+  for k, v in pairs(tbl) do
+    if type(k) == 'number' then
+      ip[k] = v
+    else
+      kv[k] = v
+    end
+  end
+  return ip, kv
 end
 
 return M
