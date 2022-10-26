0a1f36706ea47952cb3fbf4a1e9af115c982d28a
kuuote <znmxodq1@gmail.com>
2022-10-26 09:18:58 +0900

    全部Vimで展開してしまう

--- lua/vimrc/util.lua
+++ lua/vimrc/util.lua
@@ -3,7 +3,7 @@ local M = {}
 local eval = vim.eval or vim.api.nvim_eval
 
 
-local cmdSequence = eval('"\\<Cmd>"')
+local pum_vim_confirm = eval([["\<Cmd>call pum#map#confirm()\<CR>"]])
 
 function M.pum_confirm(fallback)
   if vim.fn.pumvisible() == 1 then
@@ -14,7 +14,7 @@ function M.pum_confirm(fallback)
   local ok, result = pcall(vim.fn['pum#visible'])
   if ok and result == 1 then
     if vim.call('pum#complete_info').selected ~= -1 then
-      return cmdSequence .. 'call pum#map#confirm()\13'
+      return pum_vim_confirm
     end
   end
   return fallback()
