7411a04bd7750c8a2d9966b3c892a44cdee75a20
kuuote <znmxodq1@gmail.com>
2022-11-20 19:41:41 +0000

    dein.vim用テンプレートと復元用ゔぃmrc

---
+++ dein_ginpreview.lua
@@ -0,0 +1,55 @@
+vim.g.vimrc = vim.fn.expand('<sfile>:p')
+
+local dir = '/data/vim'
+
+vim.opt.runtimepath:append('/data/vim/repos/github.com/Shougo/dein.vim')
+
+vim.g['dein#cache_directory'] = '/tmp/_dein'
+vim.g['dein#install_progress_type'] = 'floating'
+vim.g['dein#types#git#clone_depth'] = 0
+
+local function partition(tbl)
+  local ip = {}
+  local kv = {}
+  for k, v in pairs(tbl) do
+    if type(k) == 'number' then
+      ip[k] = v
+    else
+      kv[k] = v
+    end
+  end
+  return ip, kv
+end
+
+local function _use(def)
+  local args, kwargs = partition(def)
+  kwargs[vim.type_idx] = vim.types.dictionary
+  vim.call('dein#add', args[1], kwargs)
+end
+
+local function setup(fn)
+  vim.call('dein#begin', dir)
+  fn(_use)
+  vim.call('dein#end')
+end
+
+vim.keymap.set('n', 'R', function()
+  vim.call('dein#recache_runtimepath')
+  print('recached')
+end)
+for _, k in ipairs({'h', 'j', 'k', 'l'}) do
+  vim.keymap.set('n', '<C-' .. k .. '>', '<C-w>' .. k)
+end
+vim.keymap.set('n', 'Q', '<Cmd>confirm qa<CR>')
+
+setup(function(use)
+  use { 'Shougo/dein.vim', merged = 0 }
+  use { 'catppuccin/nvim', name = 'catppuccin', merged = 0 }
+  use { 'vim-denops/denops.vim' }
+  use { 'lambdalisue/gin.vim' }
+end)
+
+vim.call('dein#recache_runtimepath')
+vim.cmd('colorscheme catppuccin-latte')
+vim.cmd('source ~/.vim/plugin/gin_preview.vim')
+vim.cmd('GinPreview')
