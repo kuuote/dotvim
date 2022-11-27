32ec7b5bd07cf0235e738f59f2060dc6399c85ce
kuuote <znmxodq1@gmail.com>
2022-11-27 13:38:16 +0900

    補完切り替えメニューの導入

--- conf/plug/ddc.lua
+++ conf/plug/ddc.lua
@@ -1,18 +1,14 @@
 local au = require('vimrc.compat.autocmd').define
 local map = require('vimrc.compat.map').define
 local vimcall = require('vimrc.compat.convert').call
-local cmd = vim.cmd or vim.command
 
 local customs = {}
 
-local function change_source(name)
+local function save()
   local bufnr = vim.fn.bufnr()
   if customs[bufnr] == nil then
     customs[bufnr] = vimcall('ddc#custom#get_buffer')
   end
-  vimcall('ddc#custom#set_buffer', {
-    sources = { name },
-  })
 end
 
 local function reset()
@@ -24,32 +20,93 @@ local function reset()
 end
 
 -- resetter
-au('InsertLeave', { callback = reset })
-map('i', '<C-x><C-x>', reset)
-
-map('i', '<C-x><C-f>', function()
-  change_source('file')
-  local isk = vimcall('getbufvar', vimcall('bufnr'), '&iskeyword')
-  cmd('set isk+=.')
-  au('InsertLeave', {
+if is_nvim then
+  au({ 'CmdlineLeave', 'InsertLeave' }, {
     callback = function()
-      vimcall('setbufvar', vimcall('bufnr'), '&iskeyword', isk)
-    end
+      vim.fn.timer_start(0, function()
+        if vim.fn.mode() == 'n' then
+          reset()
+        end
+      end)
+    end,
   })
-end)
+else
+  au('SafeState', {
+    callback = function()
+      if vim.fn.mode() == 'n' then
+        reset()
+      end
+    end,
+  })
+end
 
-map('i', '<C-x><C-j>', function()
-  change_source('suconv')
-  vimcall('ddc#custom#patch_buffer', {
-    sourceOptions = {
-      suconv = {
-        converters = {},
-        matchers = {},
-        sorters = {},
-        isVolatile = true,
-        maxItems = 20,
-      }
-    },
+local function change_source(name)
+  save()
+  vimcall('ddc#custom#set_buffer', {
+    cmdlineSources = { name },
+    sources = { name },
     specialBufferCompletion = true,
   })
+end
+
+local function set_list(candidates)
+  save()
+  vimcall('ddc#custom#set_buffer', {
+    cmdlineSources = { 'list' },
+    sources = { 'list' },
+    sourceParams = {
+      list = {
+        candidates = candidates,
+      },
+    },
+  })
+end
+
+map({ 'c', 'i' }, '<C-X>', function()
+  require('vimrc.menu').menu {
+    info = 'select completion source',
+    items = {
+      ['<C-F>'] = {
+        info = 'file',
+        fn = function()
+          change_source('file')
+        end,
+      },
+      ['<C-J>'] = {
+        info = 'suconv',
+        fn = function()
+          change_source('suconv')
+          vimcall('ddc#custom#patch_buffer', {
+            sourceOptions = {
+              suconv = {
+                converters = {},
+                matchers = {},
+                sorters = {},
+                isVolatile = true,
+                maxItems = 20,
+              },
+            },
+          })
+        end,
+      },
+      ['<C-V>'] = {
+        info = 'necovim',
+        fn = function()
+          vimcall('dein#source', 'neco-vim')
+          change_source('necovim')
+        end,
+      },
+      p = {
+        info = 'plugin-name',
+        fn = function()
+          local repos = {}
+          local dein = vimcall('dein#get')
+          for _, p in pairs(dein) do
+            repos[#repos + 1] = p.repo
+          end
+          set_list(repos)
+        end,
+      },
+    },
+  }
 end)
