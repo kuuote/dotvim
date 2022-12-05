aa307f992c43caf673fd7121f29931b2d0ea497e
kuuote <znmxodq1@gmail.com>
2022-12-05 18:59:57 +0900

    ddc-s-list can accept callback

--- denops/@ddc-sources/list.ts
+++ denops/@ddc-sources/list.ts
@@ -1,18 +1,36 @@
-import { GatherArguments } from "https://deno.land/x/ddc_vim@v3.2.0/base/source.ts";
+import {
+  GatherArguments,
+  OnCompleteDoneArguments,
+} from "https://deno.land/x/ddc_vim@v3.2.0/base/source.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddc_vim@v3.2.0/types.ts";
 
 type Params = {
   candidates: string[];
+  callback: string;
 };
 
 export class Source extends BaseSource<Params> {
   override gather({ sourceParams }: GatherArguments<Params>): Promise<Item[]> {
-    return Promise.resolve(sourceParams.candidates.map((word) => ({ word })));
+    return Promise.resolve(
+      sourceParams.candidates.map((word) => ({ word, user_data: {} })),
+    );
   }
 
   override params(): Params {
     return {
       candidates: [],
+      callback: "",
     };
+  }
+
+  override async onCompleteDone(
+    args: OnCompleteDoneArguments<Params>,
+  ) {
+    if (args.sourceParams.callback !== "") {
+      await args.denops.call(
+        "denops#callback#call",
+        args.sourceParams.callback,
+      );
+    }
   }
 }
