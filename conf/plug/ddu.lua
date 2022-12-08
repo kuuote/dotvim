4d4e9b41785b0a5ee531c463d027d63dcc99ca45
kuuote <znmxodq1@gmail.com>
2022-12-08 18:21:58 +0900

    互換レイヤーは便利

--- conf/plug/ddu.lua
+++ conf/plug/ddu.lua
@@ -1,7 +1,8 @@
-local au = require('vimrc.compat.autocmd').define
-local it = require('kutil.iterate')
 local act = vim.fn['ddu#custom#action']
+local au = require('vimrc.compat.autocmd').define
 local cmd = vim.cmd or vim.command
+local it = require('kutil.iterate')
+local iter = require('vimrc.compat.convert').iter
 
 local function get_worktree(args)
   return args.items[1].action.worktree
@@ -24,7 +25,7 @@ au('User', {
     end)
     act('kind', 'git_file', 'patch', function(args)
       local worktree = get_worktree(args)
-      for _, i in ipairs(args.items) do
+      for _, i in iter(args.items) do
         cmd(('tabnew | tcd %s | GinPatch ++no-head %s'):format(worktree, i.action.path))
       end
     end)
