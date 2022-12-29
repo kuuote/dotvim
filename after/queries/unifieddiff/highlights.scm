926a8305fd09c0aa116ce0d6b1a93f860db1bdfc
kuuote <znmxodq1@gmail.com>
2022-12-29 20:10:20 +0900

    これも追従

--- after/queries/unifieddiff/highlights.scm
+++ after/queries/unifieddiff/highlights.scm
@@ -1,11 +1,22 @@
 ; from https://github.com/monaqa/tree-sitter-unifieddiff/blob/9f2f60b294e3c6dd1116e374343de25c45db86a0/queries/highlights.scm
-(git_comment) @type
-(git_index) @preproc
+(git_diff_header) @type
+
+[
+  (git_old_mode)
+  (git_new_mode)
+  (git_deleted_file_mode)
+  (git_new_file_mode)
+  (git_copy_from)
+  (git_copy_to)
+  (git_rename_from)
+  (git_rename_to)
+  (git_similarity_index)
+  (git_dissimilarity_index)
+  (git_index)
+] @preproc
 
 (from_file_line) @text.diff.delsign
 (to_file_line) @text.diff.addsign
-
-"@@" @keyword
 
 (hunk_info) @keyword @text.underline
 (hunk_comment) @text.emphasis @text.quote
