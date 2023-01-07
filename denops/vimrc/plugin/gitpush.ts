42ae3691cb020029ad020f701d1e2f6e5f0964ee
kuuote <znmxodq1@gmail.com>
2023-01-07 17:47:01 +0900

    pushできない時があるのが不便すぎた

--- denops/vimrc/plugin/gitpush.ts
+++ denops/vimrc/plugin/gitpush.ts
@@ -1,11 +1,15 @@
-import { Denops } from "../deps.ts";
+import { Denops, fn } from "../deps.ts";
+import * as mfn from "../mfn.ts";
 import * as util from "../util.ts";
 
 export async function main(denops: Denops) {
   denops.dispatcher.gitPush = async () => {
-    const path = await denops.eval(
+    let path = await denops.eval(
       "resolve(expand(isdirectory(expand('%:p')) ? '%:p' : '%:p:h'))",
     ) as string;
+    if (await Deno.stat(path).then((i) => !i.isDirectory).catch(() => true)) {
+      path = await mfn.getcwd(denops);
+    }
     const p = Deno.run({
       cmd: ["git", "push"],
       cwd: path,
