9aedf4262b60b4d4671c9f80d92ba6a5bcd06e6f
kuuote <znmxodq1@gmail.com>
2023-01-09 14:16:58 +0900

    原典に合わせておく

--- lua/vimrc/plug/lexima.lua
+++ lua/vimrc/plug/lexima.lua
@@ -5,13 +5,13 @@ local M = {}
 
 -- simulates altercmd by lexima
 -- port of https://scrapbox.io/vim-jp/lexima.vim%E3%81%A7Better_vim-altercmd%E3%82%92%E5%86%8D%E7%8F%BE%E3%81%99%E3%82%8B
-function M.altercmd(original, alternative)
+function M.altercmd(original, altanative)
   -- space
   add {
     mode = ':',
     at = [[^\('<,'>\)\?]] .. original .. [[\%#$]],
     char = '<Space>',
-    input = '<C-w>' .. alternative .. '<Space>',
+    input = '<C-w>' .. altanative .. '<Space>',
   }
 
   -- cr
@@ -19,7 +19,7 @@ function M.altercmd(original, alternative)
     mode = ':',
     at = [[^\('<,'>\)\?]] .. original .. [[\%#$]],
     char = '<CR>',
-    input = '<C-w>' .. alternative .. '<CR>',
+    input = '<C-w>' .. altanative .. '<CR>',
   }
 end
 
