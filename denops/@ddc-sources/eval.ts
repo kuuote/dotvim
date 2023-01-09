713987b07756525620883b77fba59d4097621ddd
kuuote <znmxodq1@gmail.com>
2023-01-09 18:46:51 +0900

    威張るな

---
+++ denops/@ddc-sources/eval.ts
@@ -0,0 +1,56 @@
+import {
+  BaseSource,
+  GatherArguments,
+  GetCompletePositionArguments,
+  OnCompleteDoneArguments,
+} from "/data/vim/repos/github.com/Shougo/ddc.vim/denops/ddc/base/source.ts";
+import { Item } from "/data/vim/repos/github.com/Shougo/ddc.vim/denops/ddc/types.ts";
+import { isNumber } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+
+type Params = Record<never, never>;
+
+const omitFunction = (
+  value: Record<string, unknown>,
+): Record<string, unknown> => {
+  const ret: Record<string, unknown> = {};
+  for (const key in value) {
+    if (typeof value[key] !== "function") {
+      ret[key] = value[key];
+    }
+  }
+  return ret;
+};
+
+export class Source extends BaseSource<Params> {
+  override async getCompletePosition(
+    args: GetCompletePositionArguments<Params>,
+  ): Promise<number> {
+    const result = await args.denops.eval(
+      "g:vimrc#ddc#get_complete_position(args)",
+      { args: omitFunction(args) },
+    );
+    if (isNumber(result)) {
+      this.isBytePos = true; // Vim script or Lua world is byte pos
+      return result;
+    } else {
+      this.isBytePos = false;
+      return super.getCompletePosition(args);
+    }
+  }
+
+  override gather(args: GatherArguments<Params>): Promise<Item[]> {
+    return args.denops.eval("g:vimrc#ddc#gather(args)", {
+      args: omitFunction(args),
+    }) as Promise<Item[]>;
+  }
+
+  override params(): Params {
+    return {};
+  }
+
+  override async onCompleteDone(args: OnCompleteDoneArguments<Params>) {
+    await args.denops.eval("vimrc#ddc#on_complete_done(args)", {
+      args: omitFunction(args),
+    });
+  }
+}
---
+++ denops/@ddc-sources/eval2.ts
@@ -0,0 +1,55 @@
+import { ensureNumber } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+import {
+  BaseSource,
+  GatherArguments,
+  GetCompletePositionArguments,
+OnCompleteDoneArguments,
+} from "/data/vim/repos/github.com/Shougo/ddc.vim/denops/ddc/base/source.ts";
+import { Item } from "/data/vim/repos/github.com/Shougo/ddc.vim/denops/ddc/types.ts";
+
+type Params = {
+  gather: string;
+  getCompletePosition?: string;
+  onCompleteDone?: string;
+  _?: unknown;
+};
+
+export class Source extends BaseSource<Params> {
+  override getCompletePosition(
+    args: GetCompletePositionArguments<Params>,
+  ): Promise<number> {
+    const id = args.sourceParams.getCompletePosition;
+    if (id != null) {
+      this.isBytePos = true; // Vim script or Lua world is byte pos
+      return args.denops.call("denops#callback#call", id)
+        .then(ensureNumber);
+    } else {
+      this.isBytePos = false;
+      return super.getCompletePosition(args);
+    }
+  }
+
+  override gather(args: GatherArguments<Params>): Promise<Item[]> {
+    return args.denops.call(
+      "denops#callback#call",
+      args.sourceParams.gather,
+    ) as Promise<Item[]>;
+  }
+
+  override params(): Params {
+    return {
+      gather: "",
+    };
+  }
+
+  override async onCompleteDone(
+    args: OnCompleteDoneArguments<Params>,
+  ) {
+    if (args.sourceParams.onCompleteDone != null) {
+      await args.denops.call(
+        "denops#callback#call",
+        args.sourceParams.onCompleteDone,
+      );
+    }
+  }
+}
