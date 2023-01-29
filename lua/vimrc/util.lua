8c4a718603456572bb5dba82d287e963816e3d75
kuuote <znmxodq1@gmail.com>
2023-01-29 12:05:09 +0900

    root_dirくん強化

--- lua/vimrc/util.lua
+++ lua/vimrc/util.lua
@@ -1,20 +1,60 @@
 local M = {}
 
 local cmd = vim.command or vim.cmd
-local eval = vim.eval or vim.api.nvim_eval
 
-function M.find_root(path)
+function M._get_current_dir(path)
   path = path or vim.fn.expand('%:p')
-  if path == '' then
-    path = vim.fn.getcwd() -- ファイルが開かれていない場合はcwdを使う
+  if not path:match('^/') then
+    path = vim.fn.getcwd() -- 実パスを指してなければcwdと置換
   elseif vim.fn.isdirectory(path) == 0 then
     path = vim.fn.expand('%:p:h') -- パスがファイルであればその親を取る
   end
-  local found = vim.fn.finddir('.git/..', path .. ';') -- gitリポジトリの中であれば、そのrootを取る
-  if found ~= '' then
-    path = vim.fn.fnamemodify(found, ':p')
-  end
   return path
+end
+
+local temporaly_roots = {}
+
+-- 一時的なroot dirを追加する
+function M.add_root()
+  table.insert(temporaly_roots, M._get_current_dir())
+  table.sort(temporaly_roots, function(a, b)
+    local sa = #a - #b
+    if sa == 0 then
+      return a < b
+    end
+    -- 長い方優先
+    return #a > #b
+  end)
+end
+
+-- 一時的なroot dirを削除する
+-- recursiveにtrueを渡すとカレントディレクトリ以下も全て吹き飛ばす
+function M.remove_root(recursive)
+  local path = M._get_current_dir()
+  local i = require('kutil.iterate')
+  temporaly_roots = i.new(temporaly_roots)
+    :next(i.filter(function(root)
+      if recursive then
+        return root:sub(1, #path) == path
+      else
+        return root == path
+      end
+    end))
+    :collect()
+end
+
+function M.find_root(path)
+  local dir = M._get_current_dir(path)
+  for _, r in ipairs(temporaly_roots) do
+    if dir:sub(1, #r) == r then
+      return r
+    end
+  end
+  local found = vim.fn.finddir('.git/..', dir .. ';') -- gitリポジトリの中であれば、そのrootを取る
+  if found ~= '' then
+    dir = vim.fn.fnamemodify(found, ':p')
+  end
+  return dir
 end
 
 -- 辞書の中に値を配置するやつ
