c0aa2c45727d1d887f8a92192e2a67f2c6ab330a
kuuote <znmxodq1@gmail.com>
2023-02-04 22:22:39 +0900

    調整完了

--- denops/@ddc-filters/sorter_alignment.ts
+++ denops/@ddc-filters/sorter_alignment.ts
@@ -5,6 +5,24 @@ type Params = {
   highlightMatched: string;
 };
 
+type MatchResult = {
+  matches: number[];
+  score: number;
+};
+
+function charClass(c: string): number {
+  if (c.match(/[a-z]/)) {
+    return 0;
+  }
+  if (c.match(/[A-Z]/)) {
+    return 1;
+  }
+  if (c.match(/[0-9]/)) {
+    return 2;
+  }
+  return -1;
+}
+
 export function commonString(haystack: string, needle: string): string {
   const matches: string[] = [];
   let pos = 0;
@@ -19,7 +37,11 @@ export function commonString(haystack: string, needle: string): string {
 }
 
 // Smith-Waterman algorithm based sequence alignment function
-function alignment(haystack: string, needle: string) {
+function alignment(
+  haystack: string,
+  needle: string,
+  bonus: number[],
+): MatchResult {
   const h = haystack.length + 1;
   const n = needle.length + 1;
   const matrix = Array(h * n).fill(0);
@@ -29,12 +51,13 @@ function alignment(haystack: string, needle: string) {
       const imjm = (i - 1) + (j - 1) * h;
       const ij = i + j * h;
       const match = haystack[i - 1] === needle[j - 1];
-      matrix[ij] = match ? matrix[imjm] + 1 : 0;
+      matrix[ij] = match ? matrix[imjm] + 1 + bonus[i - 1] : 0;
     }
   }
 
   // trace back
   const match = new Set<number>();
+  let score = 0;
   let hmax = haystack.length - 1;
   let npos = needle.length - 1;
   while (npos >= 0) {
@@ -48,20 +71,37 @@ function alignment(haystack: string, needle: string) {
       }
     }
 
+    // 不正確な入力を許容するので探索に失敗したら弾く
     if (haystack[hpos] !== needle[npos]) {
       npos--;
       continue;
     }
 
+    // 隣接数が欲しく、必ず1は加算されるので-1開始にしておく
+    let adjacent = -1;
     while (npos >= 0 && haystack[hpos] === needle[npos]) {
       match.add(hpos);
+      adjacent += 1;
+      score += bonus[hpos];
+
       hpos--;
       npos--;
       hmax = hpos;
     }
+    score += adjacent * 10;
   }
 
-  return [...match].sort((a, b) => a - b);
+  if (haystack === "unreftimer" || haystack === "utime") {
+    console.log(haystack);
+    console.log({
+      matches: [...match].sort((a, b) => a - b),
+      score,
+    });
+  }
+  return {
+    matches: [...match].sort((a, b) => a - b),
+    score,
+  };
 }
 
 function byteLength(input: string): number {
@@ -78,14 +118,21 @@ export class Filter extends BaseFilter<Params> {
       const word = args.sourceOptions.ignoreCase
         ? item.word.toLowerCase()
         : item.word;
-      const matches = alignment(word, commonString(word, input));
-      let score = 0;
-      // 隣接マッチスコア
-      for (let i = 1; i < matches.length; i++) {
-        if (matches[i - 1] === matches[i] - 1) {
-          score += 1;
+      const bonus = [...item.word].map((c, i, it) => {
+        // ワードらしき物の始点を加点
+        if (i === 0) {
+          return 1;
         }
-      }
+        if (charClass(c) !== charClass(it[i - 1])) {
+          return 1;
+        }
+        return 0;
+      });
+      const { matches, score } = alignment(
+        word,
+        commonString(word, input),
+        bonus,
+      );
       return {
         item,
         matches,
@@ -94,7 +141,9 @@ export class Filter extends BaseFilter<Params> {
     });
     const highlight = args.filterParams.highlightMatched;
     if (highlight !== "") {
-      return scored.sort((a, b) => b.score - a.score)
+      return scored.sort((a, b) =>
+        b.score - a.score || b.matches.length - a.matches.length
+      )
         .map((s) => ({
           ...s.item,
           highlights: (s.item.highlights ?? [])
@@ -114,7 +163,7 @@ export class Filter extends BaseFilter<Params> {
 
   params(): Params {
     return {
-      highlightMatched: "FuzzyAccent",
+      highlightMatched: "diffAdded",
     };
   }
 }
