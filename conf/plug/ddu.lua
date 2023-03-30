8a20f09b567f8a357fdbd64319262133dda16f4e
kuuote <znmxodq1@gmail.com>
2023-03-30 13:10:35 +0900

    打ってアクション

--- conf/plug/ddu.lua
+++ conf/plug/ddu.lua
@@ -3,6 +3,7 @@ local au = require('vimrc.compat.autocmd').define
 local cmd = vim.cmd or vim.command
 local it = require('kutil.iterate')
 local iter = require('vimrc.compat.convert').iter
+local vimx = require('artemis')
 
 -- kind_git_status
 
@@ -26,6 +27,32 @@ local function config()
     local worktree = get_worktree(args)
     for _, i in iter(args.items) do
       cmd(('tabnew | tcd %s | GinPatch ++no-head %s'):format(worktree, i.action.path))
+    end
+  end)
+
+  -- ui_ff
+  act('ui', 'ff', 'inputAction', function(args)
+    local item = vimx.fn['ddu#ui#get_item']()
+    local actions = vimx.fn['ddu#get_item_actions'](args.options.name, { item })
+    vimx.fn['ddc#custom#set_buffer'] {
+      cmdlineSources = { 'list' },
+      sources = { 'list' },
+      sourceOptions = {
+        list = {
+          minAutoCompleteLength = 0,
+        },
+      },
+      sourceParams = {
+        list = {
+          candidates = actions,
+        },
+      },
+    }
+    local action = vim.fn.input('action:')
+    if action ~= '' then
+      vimx.fn['ddu#ui#do_action']('itemAction', {
+        name = action,
+      })
     end
   end)
 end
