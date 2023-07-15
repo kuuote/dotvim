daebf61d6328743d2f229a0fe41fa67ad8cd64b0
kuuote <znmxodq1@gmail.com>
2023-07-15 22:59:26 +0900

    コミット忘れてるぅ

--- conf/plug/lexima.lua
+++ conf/plug/lexima.lua
@@ -18,7 +18,6 @@ local altercmd = require('vimrc.plug.lexima').altercmd
 
 altercmd('cap\\%[ture]', 'Capture')
 altercmd('d', 'DeolCurrent')
-altercmd('dd', 'DduDein')
 altercmd('ee', 'e!')
 altercmd('git', 'Gin')
 altercmd('gy', 'Gina browse --yank --exact :')
