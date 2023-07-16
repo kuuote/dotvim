8a7bea313af5ad39215f747c6f2588bc87ac417a
kuuote <znmxodq1@gmail.com>
2023-07-16 23:17:18 +0900

    setはやりすぎ

--- conf/nvim/rc/compe.lua
+++ conf/nvim/rc/compe.lua
@@ -5,11 +5,11 @@ local current = 'ddc'
 vim.keymap.set('n', '<C-c>', function()
   require('cmp')
   if current == 'ddc' then
-    vim.call('ddc#custom#set_global', {ui = 'none'})
+    vim.call('ddc#custom#patch_global', {ui = 'none'})
     vim.g.cmp = true
     current = 'cmp'
   else
-    vim.call('ddc#custom#set_global', vim.g['vimrc#ddc_config'])
+    vim.call('ddc#custom#patch_global', {ui = 'pum'})
     vim.g.cmp = false
     current = 'ddc'
   end
