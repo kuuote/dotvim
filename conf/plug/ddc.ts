cedb586dfea839ba44a53091c63a5bf6eae13269
kuuote <znmxodq1@gmail.com>
2023-08-09 13:51:34 +0900

    ddcの設定をちょちょいとやりなおした

--- conf/plug/ddc.ts
+++ conf/plug/ddc.ts
@@ -2,6 +2,10 @@ import {
   BaseConfig,
   ConfigArguments,
 } from "../../deno/ddc.vim/denops/ddc/base/config.ts";
+import * as fn from "../../deno/denops_std/denops_std/function/mod.ts";
+import * as batch from "../../deno/denops_std/denops_std/batch/mod.ts";
+
+// X<ddc-config-global>
 
 export class Config extends BaseConfig {
   override config(args: ConfigArguments): Promise<void> {
@@ -66,15 +70,19 @@ export class Config extends BaseConfig {
             isVolatile: true,
             mark: "I",
           },
+          line: {
+            matchers: ["matcher_fuzzy"],
+            sorters: ["sorter_alignment"],
+            converters: [],
+          },
           necovim: {
             mark: "V",
           },
           "nvim-lsp": {
             converters: [],
-            forceCompletionPattern: "\\.\\w*|:\\w*|->\\w*",
+            // forceCompletionPattern: "\\.\\w*|:\\w*|->\\w*",
             mark: "lsp",
             matchers: [],
-            minAutoCompleteLength: 1,
             sorters: ["sorter_alignment"],
           },
           "shell-native": {
@@ -89,11 +97,23 @@ export class Config extends BaseConfig {
             minAutoCompleteLength: 1,
             sorters: [],
           },
+          skkeleton_okuri: {
+            converters: [],
+            isVolatile: true,
+            mark: "送",
+            matchers: [],
+            maxItems: 5,
+            minAutoCompleteLength: 1,
+            sorters: [],
+          },
         },
         sourceParams: {
           file: {
             filenameChars: "[:keyword:].",
             projFromCwdMaxItems: [0],
+          },
+          "nvim-lsp": {
+            snippetEngine: 0,
           },
           "shell-native": {
             "shell": "fish",
@@ -102,6 +122,30 @@ export class Config extends BaseConfig {
         ui: "pum",
       },
     );
+    args.contextBuilder.setContextGlobal(async (denops) => {
+      const mode = await fn.mode(denops);
+      if (mode === "i") {
+        const type = await fn.getcmdwintype(denops);
+        if (type === ":") {
+          return {
+            specialBufferCompletion: true,
+            sources: ["line", "necovim"],
+          };
+        }
+      }
+      if (mode === "c") {
+        const [type, line] = await batch.collect(denops, (helper) => [
+          fn.getcmdtype(helper),
+          fn.getcmdline(helper),
+        ]);
+        if (type === ":") {
+          return {
+            cmdlineSources: ["cmdline_help", "cmdline"],
+          };
+        }
+      }
+      return {};
+    });
     return Promise.resolve();
   }
 }
