44a1ad1435e4fe320f6d6b4fa5241c46cdac3689
kuuote <znmxodq1@gmail.com>
2023-08-26 13:06:25 +0900

    profile機能してなかった cmp編(仮)

--- conf/nvim/rc/compe.lua
+++ conf/nvim/rc/compe.lua
@@ -4,25 +4,24 @@ local vimx = require('artemis')
 
 local is_available = vimx.fn.dein.is_available
 if is_available('ddc.vim') == 0 then
-  require('cmp')
+  pcall(require, 'cmp')
   vim.g.cmp = true
-  return
+else
+  local current = 'ddc'
+  local ddc_config
+
+  vim.keymap.set('n', '<C-c>', function()
+    require('cmp')
+    if current == 'ddc' then
+      ddc_config = vimx.fn.ddc.custom.get_global()
+      vimx.fn.ddc.custom.set_global({ ui = 'none' })
+      vim.g.cmp = true
+      current = 'cmp'
+    else
+      vimx.fn.ddc.custom.set_global(ddc_config)
+      vim.g.cmp = false
+      current = 'ddc'
+    end
+    print('Change completion engine to ' .. current)
+  end)
 end
-
-local current = 'ddc'
-local ddc_config
-
-vim.keymap.set('n', '<C-c>', function()
-  require('cmp')
-  if current == 'ddc' then
-    ddc_config = vimx.fn.ddc.custom.get_global()
-    vimx.fn.ddc.custom.set_global({ui = 'none'})
-    vim.g.cmp = true
-    current = 'cmp'
-  else
-    vimx.fn.ddc.custom.set_global(ddc_config)
-    vim.g.cmp = false
-    current = 'ddc'
-  end
-  print('Change completion engine to ' .. current)
-end)
