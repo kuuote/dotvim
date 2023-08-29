8f9dbfd5802c363dcf1226bc1dbde27bc73b128f
kuuote <znmxodq1@gmail.com>
2023-08-29 15:03:36 +0900

    split ngram sorter

--- denops/@ddc-filters/sorter_ngram.ts
+++ denops/@ddc-filters/sorter_ngram.ts
@@ -1,63 +1,9 @@
-import { Item } from "../../deno/ddc.vim/denops/ddc/types.ts";
 import {
   BaseFilter,
   FilterArguments,
 } from "../../deno/ddc.vim/denops/ddc/base/filter.ts";
-
-type Trie = {
-  next: Record<string, Trie>;
-};
-
-function makeTrie(input: string): Trie {
-  const root: Trie = {
-    next: {},
-  };
-  for (; input.length != 0; input = input.slice(1)) {
-    let current = root;
-    for (let i = 0; i < input.length; i++) {
-      const c = input[i];
-      current.next[c] ??= {
-        next: {},
-      };
-      current = current.next[c];
-    }
-  }
-  return root;
-}
-
-type MatchResult = {
-  start: number;
-  len: number;
-  text: string;
-};
-
-function match(input: string, trie: Trie): MatchResult[] {
-  const result: MatchResult[] = [];
-  const root = trie;
-  let current = root;
-  let len = 0;
-
-  // 終了後にチェックするのがめんどいので末尾+1まで処理させる
-  for (let i = 0; i <= input.length; i++) {
-    const c = input[i];
-    if (current.next[c] == null) {
-      if (len != 0) {
-        result.push({
-          start: i - len,
-          len,
-          text: input.slice(i - len, i),
-        });
-      }
-      len = 0;
-      current = root;
-    }
-    if (current.next[c] != null) {
-      current = current.next[c];
-      len += 1;
-    }
-  }
-  return result;
-}
+import { Item } from "../../deno/ddc.vim/denops/ddc/types.ts";
+import { makeTrie, match } from "../@kuuote/ngram.ts";
 
 function byteLength(input: string): number {
   return new TextEncoder().encode(input).length;
@@ -77,26 +23,17 @@ export class Filter extends BaseFilter<Params> {
     const input = args.sourceOptions.ignoreCase
       ? args.completeStr.toLowerCase()
       : args.completeStr;
-    const trie = makeTrie(input);
+    const needle = makeTrie(input);
     const ranked = args.items.map((item) => {
       const word = args.sourceOptions.ignoreCase
         ? item.word.toLowerCase()
         : item.word;
-      const result = match(word, trie)
-        .filter((m) => m.len >= args.filterParams.minMatchLength);
-      // 類似度と言っても、重複の多い文字列が優先されるのも嬉しくないので
-      // score matrixっぽいことをやってみる
-      const score = [...input].map(() => 0);
-      for (const r of result) {
-        const idx = input.indexOf(r.text);
-        for (let i = idx; i < idx + r.len; i++) {
-          score[i] = Math.max(score[i], r.len);
-        }
-      }
+      const result = match(word, needle, {
+        minMatchLength: args.filterParams.minMatchLength,
+      });
       return {
         item,
         result,
-        score: score.reduce((acc, s) => acc + s),
       };
     });
 
@@ -105,7 +42,7 @@ export class Filter extends BaseFilter<Params> {
       const name = "ddc-filter-sorter_ngram-" + hl_group;
       for (const { item, result } of ranked) {
         item.highlights ??= [];
-        for (const m of result) {
+        for (const m of result.matches) {
           if (args.filterParams.minMatchHighlightLength <= m.len) {
             item.highlights.push({
               name,
@@ -119,7 +56,7 @@ export class Filter extends BaseFilter<Params> {
       }
     }
 
-    return ranked.sort((a, b) => b.score - a.score)
+    return ranked.sort((a, b) => b.result.score - a.result.score)
       .map((value) => value.item);
   }
 
