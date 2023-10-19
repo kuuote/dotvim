1fdf296984fe35b42945fa1605997ba23e402046
kuuote <znmxodq1@gmail.com>
2023-10-19 19:35:52 +0900

    selector便利

---
+++ conf/plug/ddu.vim
@@ -0,0 +1,2 @@
+call ddu#custom#load_config(expand('$VIMDIR/conf/plug/ddu/main.ts'))
+call ddu#custom#load_config(expand('$VIMDIR/conf/plug/ddu/ff.ts'))
--- conf/plug/ddu/main.ts
+++ conf/plug/ddu/main.ts
@@ -2,11 +2,17 @@ import {
   ActionFlags,
   BaseConfig,
   ConfigArguments,
+  DduOptions,
 } from "../../../denops/deps/ddu.ts";
 import { ActionData as GitStatusActionData } from "/data/vim/repos/github.com/kuuote/ddu-source-git_status/denops/@ddu-kinds/git_status.ts";
+import { cmd, map } from "../../../denops/@vimrc/lib/lambda/map.ts";
 import { dduHelper } from "./lib/helper.ts";
+import { Denops, function as fn } from "../../../denops/deps/denops_std.ts";
 import { is, maybe } from "../../../denops/deps/unknownutil.ts";
 import { stdpath } from "../../../denops/deps/deno_std.ts";
+import * as sourceList from "../../../denops/@ddu-sources/list.ts";
+
+/* main section */
 
 function setupGitStatus(args: ConfigArguments) {
   const ddu = dduHelper(args.denops);
@@ -31,6 +37,7 @@ function setupGitStatus(args: ConfigArguments) {
       },
     },
     kindOptions: {
+      callback: { defaultAction: "call" },
       git_status: {
         actions: {
           commit: async () => {
@@ -117,29 +124,181 @@ function setupLocals(args: ConfigArguments) {
   });
 }
 
+function mainConfig(args: ConfigArguments) {
+  // default options
+  const defaultMatchers = ["matcher_fzf"];
+  const defaultSorters = ["sorter_fzf"];
+  // X<ddu-global>
+  args.contextBuilder.patchGlobal({
+    kindOptions: {
+      // X<ddu-kind-file>
+      file: {
+        defaultAction: "open",
+      },
+      lsp: { defaultAction: "open" },
+      lsp_codeAction: { defaultAction: "apply" },
+    },
+    sourceOptions: {
+      _: {
+        ignoreCase: true,
+        matchers: defaultMatchers,
+        sorters: defaultSorters,
+      },
+    },
+  });
+
+  setupGitStatus(args);
+  setupLocals(args);
+}
+
+/* selector section */
+
+type Promisify<T> = T | Promise<T>;
+
+// 環境から情報を収集してオプションに変える感じで
+type POptions = Promisify<Partial<DduOptions>>;
+type Collector = (denops: Denops) => POptions;
+
+async function ripgrepLive(
+  denops: Denops,
+  findPath: (denops: Denops) => Promise<string>,
+): Promise<Awaited<POptions>> {
+  // ddu-source-rg is set to lazy, load it.
+  await denops.call("dpp#source", "ddu-source-rg");
+  return {
+    name: "file:rg",
+    sources: [{
+      name: "rg",
+      options: {
+        matchers: [],
+        sorters: ["sorter_alpha_path"],
+        converters: [],
+        path: await findPath(denops),
+        volatile: true,
+      },
+    }],
+    uiParams: {
+      ff: {
+        ignoreEmpty: false,
+        autoResize: false,
+      },
+    },
+  };
+}
+
+// X<ddu-config-selector_definitions>
+const definition: Record<string, Collector> = {
+  file: async (denops) => ({
+    name: "file",
+    sources: [{
+      name: "file",
+      options: {
+        path: String(await denops.call("expand", "%:p:h")),
+        matchers: [],
+        sorters: ["sorter_alpha"],
+        converters: [],
+      },
+    }],
+  }),
+  filer: async (denops) => {
+    return {
+      name: "filer",
+      sources: [{
+        name: "file",
+        options: {
+          path: String(await denops.call("expand", "%:p:h")),
+        },
+      }],
+      ui: "filer",
+    };
+  },
+  git_branch: async (denops) => ({
+    name: "git_branch",
+    sources: [{
+      name: "git_branch",
+      options: {
+        path: String(await denops.call("expand", "%:p:h")),
+      },
+      params: {
+        remote: true,
+      },
+    }],
+  }),
+  github_repo_pull: async (denops) => {
+    // L<dpp-lazy-ddu_source_github>
+    await denops.call("dpp#source", "ddu-source-github");
+    return {
+      name: "github_repo_pull",
+      sources: [{
+        name: "github_repo_pull",
+        params: {
+          path: await denops.call("expand", "%:p:h"),
+        },
+      }],
+    };
+  },
+  live_grep: (denops) =>
+    ripgrepLive(
+      denops,
+      async (denops) => await fn.expand(denops, "%:p:h") as string,
+    ),
+  // X<ddu-config-selector-lsp>
+  lsp_codeAction: () => ({
+    name: "lsp",
+    sources: [{
+      name: "lsp_codeAction",
+    }],
+  }),
+  lsp_definition: () => ({
+    name: "lsp",
+    sources: ["lsp_definition"],
+  }),
+  lsp_references: () => ({
+    name: "lsp",
+    sources: ["lsp_references"],
+  }),
+  quickfix: () => ({
+    name: "file",
+    sources: ["qf"],
+  }),
+};
+
+async function selectorConfig(args: ConfigArguments) {
+  const { denops } = args;
+  const ddu = dduHelper(denops);
+  await map(denops, ";s", () => {
+    // コレクターを選んで実行しdduに渡す
+    ddu.start(
+      sourceList.buildOptions(
+        Object.keys(definition).sort(),
+        async (items) => {
+          if (items[0] != null) {
+            await denops.call(
+              "histadd",
+              ":",
+              "DduSelectorCall " + items[0].word,
+            );
+            ddu.start(await definition[items[0].word](denops));
+          }
+          return ActionFlags.None;
+        },
+      ),
+    );
+  }, {
+    noremap: true,
+  });
+  await cmd(denops, "DduSelectorCall", async (args) => {
+    const def = await definition[args.arg]?.(denops);
+    if (def == null) {
+      throw Error(`DduSelectorCall failed: ${args.arg}`);
+    }
+    ddu.start(def);
+  });
+}
+
 export class Config extends BaseConfig {
   async config(args: ConfigArguments) {
-    // default options
-    const defaultMatchers = ["matcher_fzf"];
-    const defaultSorters = ["sorter_fzf"];
-    // X<ddu-global>
-    args.contextBuilder.patchGlobal({
-      kindOptions: {
-        // X<ddu-kind-file>
-        file: {
-          defaultAction: "open",
-        },
-      },
-      sourceOptions: {
-        _: {
-          ignoreCase: true,
-          matchers: defaultMatchers,
-          sorters: defaultSorters,
-        },
-      },
-    });
-
-    setupGitStatus(args);
-    setupLocals(args);
+    mainConfig(args);
+    await selectorConfig(args);
   }
 }
