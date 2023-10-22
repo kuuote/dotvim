0c8e07a02b32d246799fc7a4a669229de925a191
kuuote <znmxodq1@gmail.com>
2023-10-22 22:32:55 +0900

    歴史

--- script/gitupdate/task/clean.sh
+++ script/gitupdate/task/clean.sh
@@ -3,5 +3,6 @@
 cd $1
 [[ -e .vimrc_hash ]] && exit
 [[ ! -e .git ]] && exit
-git clean -ffdx
+ls -1A --color=never | grep -v '^\.git$' | xargs rm -rf
 git restore .
+/data/vim/repos/github.com/WayneD/rsync/support/git-set-file-times
