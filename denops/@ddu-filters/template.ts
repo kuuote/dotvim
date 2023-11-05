77c810332ecb7fc64782179c8a29cbea6c3246ae
kuuote <znmxodq1@gmail.com>
2023-11-05 20:16:05 +0900

    見本大事

---
+++ denops/@ddu-filters/template.ts
@@ -0,0 +1,13 @@
+import { BaseFilter, DduItem, FilterArguments } from "../deps/ddu.ts";
+
+type Never = Record<PropertyKey, never>;
+
+export class Filter extends BaseFilter<Never> {
+  filter(args: FilterArguments<Never>): Promise<DduItem[]> {
+    return Promise.resolve(args.items);
+  }
+
+  params(): Never {
+    return {};
+  }
+}
