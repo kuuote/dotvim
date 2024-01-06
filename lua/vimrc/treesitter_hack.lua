63099777d214a7718d976924d8fa518d83614d5c
kuuote <znmxodq1@gmail.com>
2024-01-07 02:57:00 +0900

    improve treesitter hack

--- lua/vimrc/treesitter_hack.lua
+++ lua/vimrc/treesitter_hack.lua
@@ -1,13 +1,15 @@
 local treesitter_allow_filetype = {
   diff = true,
+  lua = true,
 }
 
 local ts = require('vim.treesitter')
 
 local start = ts.start
-ts.start = function(bufnr, lang)
+ts.start = function(...)
   local filetype = vim.bo[bufnr or vim.fn.bufnr()].filetype
   if treesitter_allow_filetype[filetype] then
-    return start(self, bufnr, lang)
+    vim.call('dpp#source', 'nvim-treesitter')
+    return start(...)
   end
 end
