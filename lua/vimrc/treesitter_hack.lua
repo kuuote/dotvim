ddf17317703b3447590e2ba7bdd59b4863e59b8f
kuuote <znmxodq1@gmail.com>
2024-01-20 17:41:52 +0900

    存在しない変数の記憶

--- lua/vimrc/treesitter_hack.lua
+++ lua/vimrc/treesitter_hack.lua
@@ -7,7 +7,7 @@ local ts = require('vim.treesitter')
 
 local start = ts.start
 ts.start = function(...)
-  local filetype = vim.bo[bufnr or vim.fn.bufnr()].filetype
+  local filetype = vim.bo[vim.fn.bufnr()].filetype
   if treesitter_allow_filetype[filetype] then
     vim.call('dpp#source', 'nvim-treesitter')
     return start(...)
