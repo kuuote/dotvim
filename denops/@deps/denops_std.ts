6c809271fdf59ec413090d629bf24c4ef26c7a8a
kuuote <znmxodq1@gmail.com>
2024-02-24 22:08:46 +0900

    依存忘れてた

--- denops/@deps/denops_std.ts
+++ denops/@deps/denops_std.ts
@@ -1,10 +1,11 @@
+export * as autocmd from "/data/vim/repos/github.com/vim-denops/deno-denops-std/autocmd/mod.ts";
 export {
   batch,
   collect,
 } from "/data/vim/repos/github.com/vim-denops/deno-denops-std/batch/mod.ts";
 export * as fn from "/data/vim/repos/github.com/vim-denops/deno-denops-std/function/mod.ts";
-export * as autocmd from "/data/vim/repos/github.com/vim-denops/deno-denops-std/autocmd/mod.ts";
 export * as lambda from "/data/vim/repos/github.com/vim-denops/deno-denops-std/lambda/mod.ts";
 export * as mapping from "/data/vim/repos/github.com/vim-denops/deno-denops-std/mapping/mod.ts";
-export * as option from "/data/vim/repos/github.com/vim-denops/deno-denops-std/option/mod.ts";
 export type { Denops } from "/data/vim/repos/github.com/vim-denops/deno-denops-std/mod.ts";
+export * as option from "/data/vim/repos/github.com/vim-denops/deno-denops-std/option/mod.ts";
+export * as variable from "/data/vim/repos/github.com/vim-denops/deno-denops-std/variable/mod.ts";
