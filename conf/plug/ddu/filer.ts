79eced8ef97768981c0522fad28c207345bb3152
kuuote <znmxodq1@gmail.com>
2024-07-14 22:38:00 +0900

    🌿

--- conf/plug/ddu/filer.ts
+++ conf/plug/ddu/filer.ts
@@ -1,5 +1,5 @@
 import { register } from "../../../denops/@vimrc/lib/lambda/autocmd.ts";
-import { BaseConfig, ConfigArguments } from "/data/vim/deps/ddu.ts";
+import { BaseConfig, ConfigArguments, DduItem } from "/data/vim/deps/ddu.ts";
 import { autocmd, lambda, mapping } from "/data/vim/deps/denops_std.ts";
 import { is, u } from "/data/vim/deps/unknownutil.ts";
 
@@ -27,8 +27,8 @@ async function setupFileTypeAutocmd(args: ConfigArguments) {
     _: async () => {
       await mapping.map(denops, "<CR>", action("itemAction"), nno);
       await mapping.map(denops, "d", itemAction("narrow", { path: ".." }), nno);
-      await mapping.map(denops, "h", action("collapseItem"), nno);
-      await mapping.map(denops, "l", action("expandItem"), nno);
+      await mapping.map(denops, "h", action("fernCollapse"), nno);
+      await mapping.map(denops, "l", action("fernExpandOrItemAction"), nno);
       await mapping.map(denops, "q", action("quit"), nno);
       await mapping.map(
         denops,
@@ -58,6 +58,41 @@ export class Config extends BaseConfig {
   async config(args: ConfigArguments) {
     const nvim = args.denops.meta.host === "nvim";
     args.contextBuilder.patchGlobal({
+      uiOptions: {
+        filer: {
+          actions: {
+            fernCollapse: async () => {
+              const curPos = Number(await args.denops.call("line", ".")) - 1;
+              const items = await args.denops.call(
+                "ddu#ui#get_items",
+              ) as DduItem[];
+              for (let pos = curPos; pos >= 0; pos--) {
+                if (items[pos].isTree && items[pos].__expanded) {
+                  await args.denops.call("ddu#ui#do_action", "cursorPrevious", {
+                    count: curPos - pos,
+                  });
+                  await args.denops.call("ddu#ui#do_action", "collapseItem");
+                  return;
+                }
+              }
+            },
+            fernExpandOrItemAction: async () => {
+              const item = await args.denops.call("ddu#ui#get_item") as DduItem;
+              if (item.isTree) {
+                const oldLines = await args.denops.call("line", "$");
+                await args.denops.call("ddu#ui#do_action", "expandItem");
+                const newLines = await args.denops.call("line", "$");
+                if (oldLines != newLines) {
+                  // なんか展開されてたらカーソル動かす
+                  await args.denops.call("ddu#ui#do_action", "cursorNext");
+                }
+              } else {
+                await args.denops.call("ddu#ui#do_action", "itemAction");
+              }
+            },
+          },
+        },
+      },
       uiParams: {
         filer: {
           split: nvim ? "floating" : "horizontal",
