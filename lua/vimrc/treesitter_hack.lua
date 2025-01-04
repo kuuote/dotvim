5617fb429f6ed3ae6be071b745ed7e6a3d6b3ffe
kuuote <znmxodq1@gmail.com>
2025-01-04 11:52:51 +0900

    一旦nvim-treesitterの無効化をやめてみる

--- lua/vimrc/treesitter_hack.lua
+++ lua/vimrc/treesitter_hack.lua
@@ -1,6 +1,4 @@
-local treesitter_allow_filetype = {
-  diff = true,
-  lua = true,
+local treesitter_disallow_filetype = {
 }
 
 local ts = require('vim.treesitter')
@@ -8,7 +6,7 @@ local ts = require('vim.treesitter')
 local start = ts.start
 ts.start = function(...)
   local filetype = vim.bo[vim.fn.bufnr()].filetype
-  if treesitter_allow_filetype[filetype] then
+  if not treesitter_disallow_filetype[filetype] then
     vim.call('dpp#source', 'nvim-treesitter')
     return start(...)
   end
