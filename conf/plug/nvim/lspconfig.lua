a6a1e1b785af9d7ae8c7071c973a47a46e2f0311
kuuote <znmxodq1@gmail.com>
2025-08-02 17:15:33 +0900

    formatできる時だけformatのマッピングするやつ

--- conf/plug/nvim/lspconfig.lua
+++ conf/plug/nvim/lspconfig.lua
@@ -1,11 +1,14 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   group = 'vimrc',
-  callback = function()
+  callback = function(ev)
     local opts = {
       buffer = true,
     }
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
     vim.keymap.set('n', 'ma', '<Cmd>DduSelectorCall lsp_codeAction<CR>', opts)
-    vim.keymap.set('n', 'mf', '<Cmd>lua vim.lsp.buf.format()<CR>', opts)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.keymap.set('n', 'mf', '<Cmd>lua vim.lsp.buf.format()<CR>', opts)
+    end
   end,
 })
 
