90e7651b6bc3c53b0bf14209bb864fa47fccb125
kuuote <znmxodq1@gmail.com>
2025-08-10 10:39:42 +0900

    新しいnvim-lspのあれ便利だな

--- conf/plug/nvim/lspconfig.lua
+++ conf/plug/nvim/lspconfig.lua
@@ -1,13 +1,25 @@
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = 'vimrc',
-  callback = function(ev)
-    local opts = {
-      buffer = true,
-    }
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    vim.keymap.set('n', 'ma', '<Cmd>DduSelectorCall lsp_codeAction<CR>', opts)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.keymap.set('n', 'mf', '<Cmd>lua vim.lsp.buf.format()<CR>', opts)
-    end
-  end,
+	group = 'vimrc',
+	callback = function(ev)
+		local opts = {
+			buffer = true,
+		}
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		vim.keymap.set('n', 'ma', '<Cmd>DduSelectorCall lsp_codeAction<CR>', opts)
+		if client.server_capabilities.documentFormattingProvider then
+			vim.keymap.set('n', 'mf', '<Cmd>lua vim.lsp.buf.format()<CR>', opts)
+		end
+	end,
 })
+
+local server = {
+	lua = 'lua_ls',
+	typescript = 'denols',
+}
+
+function _LspStart()
+	local name = server[vim.bo.filetype]
+	if name then
+		vim.lsp.start(vim.lsp.config[name])
+	end
+end
